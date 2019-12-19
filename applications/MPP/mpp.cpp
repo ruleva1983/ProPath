527bd2ae625be51f05656aa0655a0ee3ddc96096
Ruggero Vasile <ruggero@mag37.gfz-potsdam.de>
2019-12-19 17:14:53 +0100

    Added openmp support

--- applications/MPP/mpp.cpp
+++ applications/MPP/mpp.cpp
@@ -8,6 +8,8 @@
 #include "dijkstra.hpp"
 #include "utils.hpp"
 #include "io.hpp"
+#include <omp.h>
+
 
 #include "json_reader.hpp"
 
@@ -18,7 +20,9 @@ using namespace std;
 int main(int argc, char *argv[]) {
     
     std::string config = argv[1];
+    std::string n_threads = argv[2];
     
+    omp_set_num_threads(std::stoi(n_threads));
     string output_dir;
     vector<string> matrix_names;
     int N, r, I, J;
@@ -53,40 +57,58 @@ int main(int argc, char *argv[]) {
     int I1, I2, J1, J2;
     string output_file = index(I1, I2, J1, J2, I, J, N);
     cout << "Opening output file " + output_file + "...\n";
-	std::ofstream Pathpro(output_dir + output_file + "M=" +std::to_string(M) + "_r=" + std::to_string(r) + ".dat");
-	// Main Loop
+    std::ofstream Pathpro(output_dir + output_file + "M=" +std::to_string(M) + "_r=" + std::to_string(r) + ".dat");
+    // Main Loop
 
-	cout << "Main loop Starting" << endl;
+    cout << "Main loop Starting" << endl;
 
-	for (I = I1; I < I2; ++I) {
-		if (I % 100 == 0) {
-			cout << "Starting loop for point " << I << "." << endl;
-		}
-		for (J = J1; J < J2; ++J) {
-			int index = Mat_prod.get_index(1, M);
-			if (Mat_prod.m_prod(index).coeff(I, J) > 0) {
-				// Selecting useful points
-				std::vector<int> Count(M - 1);
-				std::vector<std::vector<int>> U;
-				useful_points(Mat_prod, M, N, I, J, Count, U);
-
-				///// Initial Djkstra Algorithm Calling /////
-				std::vector<int> PathMAX;
-				double ProMAX = Dijkstra(Mat, 0, M, I, J, M, U, Count, PathMAX);
-
-				// Writing into file stream Pathpro
-				if (r == 0) {
-					int index = Mat_prod.get_index(1, M);
-					for (int m = 0; m <= M; ++m)
-                        Pathpro << PathMAX[m] << " ";
-                    Pathpro << ProMAX << " " << Full_mat.coeff(I, J) << " "
-                            << Mat_prod.m_prod(index).coeff(I, J) << " "
-							<< ProMAX / Full_mat.coeff(I, J) << endl;
-				}
-
-            }
+    for (I = I1; I < I2; ++I) {
+        if (I % 100 == 0) {
+            cout << "Starting loop for point " << I << "." << endl;
         }
-}
+        
+        std::vector<std::vector<int>> Paths;
+        std::vector<double> Probabilities;
+        std::vector<int> nodes;
+        
+        #pragma omp parallel for
+        for (J = J1; J < J2; ++J) {
+            int index = Mat_prod.get_index(1, M);
+            if (Mat_prod.m_prod(index).coeff(I, J) > 0) {
+                // Selecting useful points
+                std::vector<int> Count(M - 1);
+                std::vector<std::vector<int>> U;
+                
+                //TODO Put useful points inside Dijkstra
+                useful_points(Mat_prod, M, N, I, J, Count, U);
+
+                ///// Initial Djkstra Algorithm Calling /////
+                std::vector<int> PathMAX;
+                double ProMAX = Dijkstra(Mat, 0, M, I, J, M, U, Count, PathMAX);
+                #pragma omp critical
+                Paths.push_back(PathMAX);
+                #pragma omp critical
+                Probabilities.push_back(ProMAX);
+                #pragma omp critical
+                nodes.push_back(J);
+
+                }
+
+        }
+        int index = Mat_prod.get_index(1, M);
+        int j = 0;
+        for (auto J : nodes){
+            for (int m = 0; m <= M; ++m) 
+                Pathpro << Paths[j][m] << " ";
+            Pathpro << Probabilities[j] << " " << Full_mat.coeff(I, J) << " " << Mat_prod.m_prod(index).coeff(I, J)
+            << " " << Probabilities[j] / Full_mat.coeff(I, J) << endl;
+            j++;
+        }
+            
+            
+            
+    }
+
 
     Pathpro.close();
     std::cout << "Time for Dijkstra: " << t.elapsed() << " sec " << std::endl;
--- include/dijkstra.hpp
+++ include/dijkstra.hpp
@@ -9,12 +9,12 @@ using namespace std;
 
 
 double Dijkstra(Mat_vector&, int, int, int, int, int,
-		std::vector<std::vector<int>>& U, std::vector<int>&,
-		std::vector<int>&);
+        std::vector<std::vector<int>>& U, std::vector<int>&,
+        std::vector<int>&);
 
 
 
 void useful_points(Mat_prod_access&, int, int, int, int,
-		std::vector<int>&, std::vector<std::vector<int>>&);
+        std::vector<int>&, std::vector<std::vector<int>>&);
 
 #endif
